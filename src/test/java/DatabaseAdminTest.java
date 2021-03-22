da3b58eded08072b53b4e8a1f822a237c5b1cb30
Anna Cunningham <ac232@hotmail.co.uk>
2021-03-22 14:38:02 +0000

    databaseAdmin tests pass

---
+++ src/test/java/DatabaseAdminTest.java
@@ -0,0 +1,42 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.techStaff.DatabaseAdmin;
+
+import static org.junit.Assert.assertEquals;
+
+public class DatabaseAdminTest {
+
+    DatabaseAdmin databaseAdmin;
+
+    @Before
+    public void before(){
+        databaseAdmin = new DatabaseAdmin("Erin", "LK989C00L", 25000.50);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Erin", databaseAdmin.getName());
+    }
+
+    @Test
+    public void hasNationalInsuranceNumber(){
+        assertEquals("LK989C00L", databaseAdmin.getNationalInsuranceNumber());
+    }
+
+    @Test
+    public void hasSalary(){
+        assertEquals(25000.50, databaseAdmin.getSalary(), 0.01);
+    }
+
+
+    @Test
+    public void canRaiseSalary(){
+        databaseAdmin.raiseSalary(500.00);
+        assertEquals(25500.50, databaseAdmin.getSalary(), 0.01);
+    }
+
+    @Test
+    public void canGetBonusAmount(){
+        assertEquals(250.01, databaseAdmin.payBonus(), 0.01);
+    }
+}
